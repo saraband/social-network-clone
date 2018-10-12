c2e1b2b7a4a12b841c15bce9a34b3ed647bb6441
Yassine Hermellin <yhe@radionomy.com>
2018-10-12 15:08:00 +0200

    withLoader HOC

--- src/browser/hocs/WithLoader/index.js
+++ src/browser/hocs/WithLoader/index.js
@@ -2,56 +2,56 @@ import React, { PureComponent } from 'react'
 import PlainLoader from 'COMPONENTS/Loaders/PlainLoader';
 
 export default ({
-	Loader = PlainLoader,
-	loaderProps = {},
-	where = 'before'
+  Loader = PlainLoader,
+  loaderProps = {},
+  where = 'before'
 }) => Component => {
-	let renderComponentWithLoader;
+  let renderComponentWithLoader;
 
-	switch (where) {
-		case 'before':
-			renderComponentWithLoader = (props) => (
-				<React.Fragment>
-					<Loader {...loaderProps} />
-					<Component {...props} />
-				</React.Fragment>
-			)
-			break;
+  switch (where) {
+    case 'before':
+      renderComponentWithLoader = (props) => (
+        <React.Fragment>
+          <Loader {...loaderProps} />
+          <Component {...props} />
+        </React.Fragment>
+      )
+      break;
 
-		case 'after':
-			renderComponentWithLoader = (props) => (
-				<React.Fragment>
-					<Component {...props} />
-					<Loader {...loaderProps} />
-				</React.Fragment>
-			)
-			break;
+    case 'after':
+      renderComponentWithLoader = (props) => (
+        <React.Fragment>
+          <Component {...props} />
+          <Loader {...loaderProps} />
+        </React.Fragment>
+      )
+      break;
 
-		case 'inside':
-			renderComponentWithLoader = (props) => (
-				<Component {...props}>
-					<Loader {...loaderProps} />
-				</Component>
-			)
-			break;
+    case 'inside':
+      renderComponentWithLoader = (props) => (
+        <Component {...props}>
+          <Loader {...loaderProps} />
+        </Component>
+      )
+      break;
 
-		default:
-			console.error(`'${where}' is not a possible option for property 'where' in withLoader HOC`)
-			renderComponentWithLoader = (props) => <Component {...props} />
-			break;
-	}
+    default:
+      console.error(`'${where}' is not a possible option for property 'where' in withLoader HOC`)
+      renderComponentWithLoader = (props) => <Component {...props} />
+      break;
+  }
 
-	return class extends PureComponent {
-		render() {
-			const {
-				isLoading,
-				...rest
-			} = this.props
+  return class extends PureComponent {
+    render() {
+      const {
+        isLoading,
+        ...rest
+      } = this.props
 
-			if (!isLoading)
-				return <Component {...rest} />
+      if (!isLoading)
+        return <Component {...rest} />
 
-			return renderComponentWithLoader(rest);
-		}
-	}
+      return renderComponentWithLoader(rest);
+    }
+  }
 }
